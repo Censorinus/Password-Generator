284cde22db537d5fe7b4ae77e3508388d6720a6f
Censorinus <jgrant.2901@gmail.com>
2021-11-02 17:07:58 -0400

    Second Commit

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment code here
 
+function generatePassword() {
+
+  return "Password goes here";
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
